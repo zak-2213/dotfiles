314e0acfb80e3c8e0f97acd88db8d9bcf2ba2303
Zak <57948993+zak-2213@users.noreply.github.com>
2025-05-11 19:41:53 +0200

    changes

--- nvim/.config/nvim/after/plugin/colours.lua
+++ nvim/.config/nvim/after/plugin/colours.lua
@@ -2,15 +2,70 @@ function SetColours(colour)
 	colour = colour or "catppuccin-frappe"
 	vim.cmd.colorscheme(colour)
 
-	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- Clear background for all relevant highlight groups
+	local groups = {
+		"ColorColumn",
+		"Conceal",
+		"Cursor",
+		"CursorIM",
+		"CursorColumn",
+		"CursorLine",
+		"Directory",
+		"DiffAdd",
+		"DiffChange",
+		"DiffDelete",
+		"DiffText",
+		"EndOfBuffer",
+		"ErrorMsg",
+		"VertSplit",
+		"Folded",
+		"FoldColumn",
+		"SignColumn",
+		"IncSearch",
+		"LineNr",
+		"LineNrAbove",
+		"LineNrBelow",
+		"CursorLineNr",
+		"MatchParen",
+		"ModeMsg",
+		"MoreMsg",
+		"NonText",
+		"Normal",
+		"NormalFloat",
+		"NormalNC",
+		"Pmenu",
+		"PmenuSel",
+		"PmenuSbar",
+		"PmenuThumb",
+		"Question",
+		"QuickFixLine",
+		"Search",
+		"SpecialKey",
+		"SpellBad",
+		"SpellCap",
+		"SpellLocal",
+		"SpellRare",
+		"StatusLine",
+		"StatusLineNC",
+		"TabLine",
+		"TabLineFill",
+		"TabLineSel",
+		"Terminal",
+		"Title",
+		"Visual",
+		"VisualNOS",
+		"WarningMsg",
+		"Whitespace",
+		"WildMenu",
+	}
+
+	for _, group in ipairs(groups) do
+		vim.api.nvim_set_hl(0, group, { bg = "none" })
+	end
+
+	-- Additional settings for better transparency
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "none" })
-	vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-	vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TabLineSelect", { bg = "none" })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 end
 
 SetColours()
